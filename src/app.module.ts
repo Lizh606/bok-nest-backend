f2fc809e9de20bb53089ac93bea3f975706d4f3c
李泽航 <lizh@dist.com.cn>
2024-01-26 13:55:00 +0800

    feat:typeorm cli数据库以及生产配置重构

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,18 @@
  * @Description: 请填写简介
  */
 import { Global, Logger, Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-// import * as dotenv from 'dotenv';
+import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
 // import * as config from 'config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
 import { LoggerModule } from 'nestjs-pino';
+import { connectionParams } from '../ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import configuration from './configuration';
-import { ConfigEnum } from './enum/config.enum';
+import { LogsModule } from './logs/logs.module';
 import { ResourceModule } from './resource/resource.module';
 import { UserModule } from './user/user.module';
-import { LogsModule } from './logs/logs.module';
 // config插件获取获取（自动合并）
 // console.log(config.get('database'), config);
 const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
@@ -30,39 +29,29 @@ const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
     ResourceModule,
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [configuration],
+      // load: [configuration],
       validationSchema: Joi.object({
         NODE_ENV: Joi.string()
           .valid('production', 'development')
           .default('development'),
         DB_PORT: Joi.number().default(3306),
-        DB_HOST: Joi.string().ip(),
+        DB_HOST: Joi.alternatives().try(
+          Joi.string().ip(),
+          Joi.string().domain(),
+        ),
         DB_TYPE: Joi.string().valid('mysql', 'postgres').default('mysql'),
         DB_DATABASE: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_SYNC: Joi.boolean().default(false),
+        LOG_LEVEL: Joi.string(),
+        LOG_ON: Joi.boolean(),
       }),
       envFilePath,
-      // load: [() => dotenv.config({ path: '.env' })],
+      load: [() => dotenv.config({ path: '.env' })],
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) =>
-        ({
-          type: configService.get(ConfigEnum.DB_TYPE),
-          host: configService.get(ConfigEnum.DB_HOST),
-          port: configService.get(ConfigEnum.DB_PORT),
-          username: configService.get(ConfigEnum.DB_USERNAME),
-          password: configService.get(ConfigEnum.DB_PASSWORD),
-          database: configService.get(ConfigEnum.DB_DATABASE),
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          // 同步本地schema和数据库
-          synchronize: true,
-          // logging: process.env.NODE_ENV === 'development',
-        }) as TypeOrmModuleOptions,
-    }),
+    TypeOrmModule.forRoot(connectionParams),
+
     LoggerModule.forRoot({
       pinoHttp: {
         transport: {
--- src/logs/logs.module.ts
+++ src/logs/logs.module.ts
@@ -7,7 +7,21 @@ import { Console } from 'winston/lib/winston/transports';
 import { LogEnum } from '../enum/config.enum';
 import { LogsController } from './logs.controller';
 import { LogsService } from './logs.service';
-
+function createDailyRotateTrasnport(level: string, filename: string) {
+  return new DailyRotateFile({
+    level,
+    dirname: 'logs',
+    filename: `${filename}-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.simple(),
+    ),
+  });
+}
 @Module({
   imports: [
     WinstonModule.forRootAsync({
@@ -21,38 +35,14 @@ import { LogsService } from './logs.service';
           ),
         });
 
-        const dailyTransports = new DailyRotateFile({
-          level: 'warn',
-          dirname: 'logs',
-          filename: 'application-%DATE%.log',
-          datePattern: 'YYYY-MM-DD-HH',
-          zippedArchive: true,
-          maxSize: '20m',
-          maxFiles: '14d',
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.simple(),
-          ),
-        });
-
-        const dailyInfoTransports = new DailyRotateFile({
-          level: configService.get(LogEnum.LOG_LEVEL),
-          dirname: 'logs',
-          filename: 'info-%DATE%.log',
-          datePattern: 'YYYY-MM-DD-HH',
-          zippedArchive: true,
-          maxSize: '20m',
-          maxFiles: '14d',
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.simple(),
-          ),
-        });
         return {
           transports: [
             consoleTransports,
             ...(configService.get(LogEnum.LOG_ON)
-              ? [dailyInfoTransports, dailyTransports]
+              ? [
+                  createDailyRotateTrasnport('info', 'application'),
+                  createDailyRotateTrasnport('warn', 'error'),
+                ]
               : []),
           ],
         } as WinstonModuleOptions;
